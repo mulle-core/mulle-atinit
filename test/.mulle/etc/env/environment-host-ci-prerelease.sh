edaab6f575e1db3480d128b93027e92e78c49975
Nat! <nat@mulle-kybernetik.com>
2022-01-11 02:31:22 +0100

    prerelease

--- test/.mulle/etc/env/environment-host-ci-prerelease.sh
+++ test/.mulle/etc/env/environment-host-ci-prerelease.sh
@@ -70,3 +70,21 @@ export MULLE_DLFCN_URL="https://github.com/mulle-core/mulle-dlfcn.git"
 export MULLE_DLFCN_BRANCH="prerelease"
 
 
+#
+# dlfcn-win32 nodetype: ${DLFCN_WIN32_NODETYPE:-tar}
+#
+export DLFCN_WIN32_NODETYPE="git"
+
+
+#
+# dlfcn-win32 url: ${DLFCN_WIN32_URL:-https://github.com/mulle-core/dlfcn-win32/archive/refs/tags/${MULLE_TAG}.tar.gz}
+#
+export DLFCN_WIN32_URL="https://github.com/mulle-core/dlfcn-win32.git"
+
+
+#
+# dlfcn-win32 branch: ${DLFCN_WIN32_BRANCH}
+#
+export DLFCN_WIN32_BRANCH="prerelease"
+
+
